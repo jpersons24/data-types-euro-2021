4c80480927d3e61d6718d57e8b54d8355308ead7
Jakob Persons <jakobep24@gmail.com>
2021-06-21 12:17:29 -0400

    initial commit

---
+++ main.kt
@@ -0,0 +1,15 @@
+fun main() {
+  // ordered list of every country
+
+  // 4 teams per group
+
+  // 6 maps with "team" to "points" - String to Int
+
+  // Two teams with most point in each group move to knockout set
+
+  // 6 3rd place teams placed in list ordered by number of points
+
+  // top 4 3rd place teams added to knockout set
+
+  // if knockout set has 16 teams...create first round knockout matchups randomly
+}
